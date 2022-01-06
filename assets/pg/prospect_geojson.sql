8643850e2a23e80a01c3beca9a8c0496515f5f39
Christopher Ramey <christopher.ramey@alaska.gov>
2022-01-05 16:54:55 -0900

    Further improvements to prospect popup

--- assets/pg/prospect_geojson.sql
+++ assets/pg/prospect_geojson.sql
@@ -5,11 +5,13 @@ SELECT jsonb_build_object(
 			'type', 'Feature',
 			'id', b.borehole_id,
 			'geometry', ST_AsGeoJSON(p.geog, 5, 0)::jsonb,
-			'properties', jsonb_build_object(
+			'properties', jsonb_strip_nulls(jsonb_build_object(
 				'borehole_id', b.borehole_id,
-				'name', b.name
-			)
-		))
+				'name', b.name,
+				'alt_names', b.alt_names,
+				'completion_date', to_char(b.completion_date, 'MM/DD/YY')
+			))
+		) ORDER BY LOWER(b.name) DESC)
 		FROM borehole AS b
 		JOIN borehole_point AS bp
 			ON bp.borehole_id = b.borehole_id
