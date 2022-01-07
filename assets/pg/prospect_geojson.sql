3c5fa7e68a45e1d9184481671787d6cbdacbbd2b
Christopher Ramey <christopher.ramey@alaska.gov>
2022-01-07 07:07:17 -0900

    Fix display when prospect without spatial data

--- assets/pg/prospect_geojson.sql
+++ assets/pg/prospect_geojson.sql
@@ -1,7 +1,9 @@
 SELECT jsonb_build_object(
 	'type', 'FeatureCollection',
-	'features', ((
-		SELECT jsonb_agg(jsonb_build_object(
+	'features', q.features
+) AS geojson
+FROM (
+	SELECT jsonb_agg(jsonb_build_object(
 			'type', 'Feature',
 			'id', b.borehole_id,
 			'geometry', ST_AsGeoJSON(p.geog, 5, 0)::jsonb,
@@ -11,12 +13,13 @@ SELECT jsonb_build_object(
 				'alt_names', b.alt_names,
 				'completion_date', to_char(b.completion_date, 'MM/DD/YY')
 			))
-		) ORDER BY LOWER(b.name) DESC)
-		FROM borehole AS b
-		JOIN borehole_point AS bp
-			ON bp.borehole_id = b.borehole_id
-		JOIN point AS p
-			ON p.point_id = bp.point_id
-		WHERE b.prospect_id = $1
-	))
-) as geojson
+		) ORDER BY LOWER(b.name)
+	) AS features
+	FROM borehole AS b
+	JOIN borehole_point AS bp
+		ON bp.borehole_id = b.borehole_id
+	JOIN point AS p
+		ON p.point_id = bp.point_id
+	WHERE b.prospect_id = $1
+) AS q
+WHERE q.features IS NOT NULL
