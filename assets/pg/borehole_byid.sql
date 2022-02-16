79f42d92a4f588f928585218c7c0f73b6f3ffc7d
Christopher Ramey <christopher.ramey@alaska.gov>
2022-02-16 14:40:48 -0900

    Add measured depth to borehole

--- assets/pg/borehole_byid.sql
+++ assets/pg/borehole_byid.sql
@@ -1,7 +1,9 @@
 SELECT p.prospect_id, p.name AS prospect_name,
 	p.alt_names AS alt_prospect_names, p.ardf_number,
 	b.borehole_id, b.name, b.alt_names,
-	b.is_onshore, b.completion_date
+	b.is_onshore, b.completion_date,
+	b.measured_depth::double precision,
+	COALESCE(measured_depth_unit::text, 'ft') AS measured_depth_unit
 FROM borehole AS b
 LEFT OUTER JOIN prospect AS p
 	ON p.prospect_id = b.prospect_id
