375f75092c3b408d89c2614003291897630f2032
Tommy Folan <tommy.folan@alaska.gov>
2022-05-03 08:53:07 -0800

    Move date manipulation to web/well_json.go

--- web/well_json.go
+++ web/well_json.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	dbf "gmc/db/flag"
 	"net/http"
+	"time"
 )
 
 func (srv *Server) ServeWellJSON(id int, w http.ResponseWriter, r *http.Request) {
 	flags := dbf.ALL_NOPRIVATE
-	welljson, err := srv.DB.GetWellJSON(id, flags)
+	welljson, err := srv.DB.GetWell(id, flags)
 	if err != nil {
 		http.Error(
 			w, fmt.Sprintf("Error: %s", err.Error()),
@@ -23,6 +24,23 @@ func (srv *Server) ServeWellJSON(id int, w http.ResponseWriter, r *http.Request)
 		http.Error(w, "Well ID not found", http.StatusNotFound)
 		return
 	}
+
+	cd, ok := welljson["completion_date"].(time.Time)
+	if !ok {
+		delete(welljson, "completion_date")
+	} else {
+		fcd := cd.Format("01-02-2006")
+		welljson["completion_date"] = &fcd
+	}
+
+	sd, ok := welljson["spud_date"].(time.Time)
+	if !ok {
+		delete(welljson, "spud_date")
+	} else {
+		fsd := sd.Format("01-02-2006")
+		welljson["spud_date"] = &fsd
+	}
+
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	js, err := json.Marshal(welljson)
