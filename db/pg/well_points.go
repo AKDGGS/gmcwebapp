b69b2258ec1c599707460f9476850209eaddf55c
Tommy Folan <tommy.folan@alaska.gov>
2022-05-05 12:46:35 -0800

    Remove testing code

--- db/pg/well_points.go
+++ db/pg/well_points.go
@@ -1,8 +1,5 @@
 package pg
 
-import "fmt"
-
 func (pg *Postgres) GetWellPoints() ([]map[string]interface{}, error) {
-	fmt.Println(pg.queryRows("pg/well_points.sql"))
 	return pg.queryRows("pg/well_points.sql")
 }
