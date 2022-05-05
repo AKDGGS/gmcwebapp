751f93357b4a25160d7cf1ebce8bb8e58833efe3
Tommy Folan <tommy.folan@alaska.gov>
2022-05-05 08:02:26 -0800

    Add code to handle well_points.go when gzip is unavailable

--- web/well_points.go
+++ web/well_points.go
@@ -18,6 +18,7 @@ func (srv *Server) ServeWellPoints(name string, w http.ResponseWriter, r *http.R
 		)
 		return
 	}
+
 	// If no details were returned, throw a 404
 	if pts == nil {
 		http.Error(w, "Point list not found", http.StatusNotFound)
@@ -32,56 +33,60 @@ func (srv *Server) ServeWellPoints(name string, w http.ResponseWriter, r *http.R
 		)
 		return
 	}
-	var gzok bool
+
 	var content *[]byte
-	// 860 bytes is Akamai's recommended minimum for gzip
-	// so only bother to gzip files greater than 860 bytes
-	if len(js) > 860 {
-		var buf bytes.Buffer
-		gz, err := gzip.NewWriterLevel(&buf, gzip.BestCompression)
-		if err != nil {
-			http.Error(
-				w, fmt.Sprintf("gzip error: %s", err.Error()),
-				http.StatusInternalServerError,
-			)
-			return
-		}
-		defer gz.Close()
+	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		var gzok bool
+		// 860 bytes is Akamai's recommended minimum for gzip
+		// so only bother to gzip files greater than 860 bytes
+		if len(js) > 860 {
+			var buf bytes.Buffer
+			gz, err := gzip.NewWriterLevel(&buf, gzip.BestCompression)
+			if err != nil {
+				http.Error(
+					w, fmt.Sprintf("gzip error: %s", err.Error()),
+					http.StatusInternalServerError,
+				)
+				return
+			}
+			defer gz.Close()
 
-		if _, err := gz.Write(js); err != nil {
-			http.Error(
-				w, fmt.Sprintf("gz write error: %s", err.Error()),
-				http.StatusInternalServerError,
-			)
-			return
-		}
+			if _, err := gz.Write(js); err != nil {
+				http.Error(
+					w, fmt.Sprintf("gz write error: %s", err.Error()),
+					http.StatusInternalServerError,
+				)
+				return
+			}
 
-		if err := gz.Flush(); err != nil {
-			http.Error(
-				w, fmt.Sprintf("gz write error: %s", err.Error()),
-				http.StatusInternalServerError,
-			)
-			return
-		}
-		var gzc []byte
-		// Only accept gzip if it's less than the original in size
-		if buf.Len() > 0 && buf.Len() < len(js) {
-			gzc = buf.Bytes()
-		}
+			if err := gz.Flush(); err != nil {
+				http.Error(
+					w, fmt.Sprintf("gz write error: %s", err.Error()),
+					http.StatusInternalServerError,
+				)
+				return
+			}
+			var gzc []byte
+			// Only accept gzip if it's less than the original in size
+			if buf.Len() > 0 && buf.Len() < len(js) {
+				gzc = buf.Bytes()
+			}
 
-		gzok = strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") && &gzc != nil
+			gzok = strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") && &gzc != nil
+			if gzok {
+				content = &gzc
+			}
+		}
+		w.Header().Set("Content-Type", "application/json")
 		if gzok {
-			content = &gzc
+			w.Header().Set("Content-Encoding", "gzip")
 		}
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", len(*content)))
+		w.Write(*content)
 	} else {
 		content = &js
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", len(*content)))
+		w.Write(*content)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if gzok {
-		w.Header().Set("Content-Encoding", "gzip")
-	}
-	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(*content)))
-	w.Write(*content)
-
 }
