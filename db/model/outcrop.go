e8545778df597cc4f11df195eb6b4149c3bfcc03
Tommy Folan <tommy.folan@alaska.gov>
2022-11-23 08:39:12 -0900

    Change field name from OutcropNumber to Number in model/outcrop.go

--- db/model/outcrop.go
+++ db/model/outcrop.go
@@ -1,14 +1,14 @@
 package model
 
 type Outcrop struct {
-	ID            int32                  `json:"outcrop_id"`
-	Name          string                 `json:"name"`
-	IsOnshore     bool                   `json:"is_onshore"`
-	OutcropNumber string                 `json:"outcrop_number"`
-	Year          int16                  `json:"year"`
-	Stash         map[string]interface{} `json:"stash"`
-	Note          []Note                 `json:"note"`
-	URL           []URL                  `json:"url"`
-	Organization  []Organization         `json:"organization"`
-	File          []File                 `json:"file"`
+	ID           int32                  `json:"outcrop_id"`
+	Name         string                 `json:"name"`
+	IsOnshore    bool                   `json:"is_onshore"`
+	Number       string                 `json:"outcrop_number"`
+	Year         int16                  `json:"year"`
+	Stash        map[string]interface{} `json:"stash"`
+	Note         []Note                 `json:"note"`
+	URL          []URL                  `json:"url"`
+	Organization []Organization         `json:"organization"`
+	File         []File                 `json:"file"`
 }
