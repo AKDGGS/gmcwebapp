f3b248023455e2b31ba32e737402de179e230f8b
Tommy Folan <tommy.folan@alaska.gov>
2022-11-29 08:34:28 -0900

    Make array fields plural

--- db/model/outcrop.go
+++ db/model/outcrop.go
@@ -1,17 +1,17 @@
 package model
 
 type Outcrop struct {
-	ID           int32                    `json:"outcrop_id"`
-	Name         string                   `json:"name"`
-	IsOnshore    bool                     `json:"is_onshore"`
-	Number       string                   `json:"outcrop_number"`
-	Year         int16                    `json:"year"`
-	Stash        map[string]interface{}   `json:"stash"`
-	Keywords     []map[string]interface{} `json:"keywords"`
-	GeoJSON      map[string]interface{}   `json:"geojson"`
-	Quadrangle   []Quadrangle             `json:"quadrangle"`
-	Note         []Note                   `json:"note"`
-	URL          []URL                    `json:"url"`
-	Organization []Organization           `json:"organization"`
-	File         []File                   `json:"file"`
+	ID            int32                    `json:"outcrop_id"`
+	Name          string                   `json:"name"`
+	IsOnshore     bool                     `json:"is_onshore"`
+	Number        string                   `json:"outcrop_number"`
+	Year          int16                    `json:"year"`
+	Stash         map[string]interface{}   `json:"stash"`
+	Keywords      []map[string]interface{} `json:"keywords"`
+	GeoJSON       map[string]interface{}   `json:"geojson"`
+	Quadrangles   []Quadrangle             `json:"quadrangles"`
+	Notes         []Note                   `json:"notes"`
+	URLs          []URL                    `json:"urls"`
+	Organizations []Organization           `json:"organizations"`
+	Files         []File                   `json:"files"`
 }
