a97714482046255d8a71372dbdb3f9502532ac9d
Tommy Folan <tommy.folan@alaska.gov>
2023-02-07 10:23:01 -0900

    Match field order to sql column order

--- db/model/outcrop.go
+++ db/model/outcrop.go
@@ -1,10 +1,17 @@
 package model
 
+import (
+	"time"
+)
+
 type Outcrop struct {
 	ID             int32                  `json:"outcrop_id"`
 	Name           string                 `json:"name"`
-	IsOnshore      bool                   `json:"is_onshore"`
 	Number         string                 `json:"outcrop_number"`
+	IsOnshore      bool                   `json:"is_onshore"`
+	EnteredDate    *time.Time             `json:"entered_date"`
+	ModifiedDate   *time.Time             `json:"modified_date"`
+	ModifiedUser   string                 `json:"modified_user"`
 	Year           int16                  `json:"year"`
 	Stash          map[string]interface{} `json:"stash"`
 	KeywordSummary []KeywordSummary       `json:"keywords"`
