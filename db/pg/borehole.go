60eaef8ac6964fec20cd9b50c376f5a2ada22c0c
Tommy Folan <tommy.folan@alaska.gov>
2023-04-19 10:33:32 -0800

    Fix for empty versus nil object and returns a zero if nothing is returned or a one if a row is returned

--- db/pg/borehole.go
+++ db/pg/borehole.go
@@ -19,105 +19,108 @@ func (pg *Postgres) GetBorehole(id int, flags int) (*model.Borehole, error) {
 	}
 	defer rows.Close()
 	borehole := model.Borehole{}
-	rowToStruct(rows, &borehole)
+	mainRowCount := rowToStruct(rows, &borehole)
 
-	if (flags & dbf.PROSPECT) != 0 {
-		q, err = assets.ReadString("pg/prospect/by_borehole_id.sql")
-		if err != nil {
-			return nil, err
+	if mainRowCount > 0 {
+		if (flags & dbf.PROSPECT) != 0 {
+			q, err = assets.ReadString("pg/prospect/by_borehole_id.sql")
+			if err != nil {
+				return nil, err
+			}
+			r, err := pg.pool.Query(context.Background(), q, id)
+			if err != nil {
+				return nil, err
+			}
+			rowToStruct(r, &borehole.Prospect)
 		}
-		r, err := pg.pool.Query(context.Background(), q, id)
-		if err != nil {
-			return nil, err
-		}
-		rowToStruct(r, &borehole.Prospect)
-	}
 
-	if (flags & dbf.FILES) != 0 {
-		q, err = assets.ReadString("pg/file/by_borehole_id.sql")
-		if err != nil {
-			return nil, err
+		if (flags & dbf.FILES) != 0 {
+			q, err = assets.ReadString("pg/file/by_borehole_id.sql")
+			if err != nil {
+				return nil, err
+			}
+			r, err := pg.pool.Query(context.Background(), q, id)
+			if err != nil {
+				return nil, err
+			}
+			rowToStruct(r, &borehole.Files)
 		}
-		r, err := pg.pool.Query(context.Background(), q, id)
-		if err != nil {
-			return nil, err
+		if (flags & dbf.INVENTORY_SUMMARY) != 0 {
+			q, err = assets.ReadString("pg/keyword/group_by_borehole_id.sql")
+			if err != nil {
+				return nil, err
+			}
+			r, err := pg.pool.Query(context.Background(), q, id, ((flags & dbf.PRIVATE) == 0))
+			if err != nil {
+				return nil, err
+			}
+			rowToStruct(r, &borehole.KeywordSummary)
 		}
-		rowToStruct(r, &borehole.Files)
+		if (flags & dbf.ORGANIZATION) != 0 {
+			q, err = assets.ReadString("pg/organization/by_borehole_id.sql")
+			if err != nil {
+				return nil, err
+			}
+			r, err := pg.pool.Query(context.Background(), q, id)
+			if err != nil {
+				return nil, err
+			}
+			rowToStruct(r, &borehole.Organizations)
+		}
+		if (flags & dbf.URLS) != 0 {
+			q, err = assets.ReadString("pg/url/by_borehole_id.sql")
+			if err != nil {
+				return nil, err
+			}
+			r, err := pg.pool.Query(context.Background(), q, id)
+			if err != nil {
+				return nil, err
+			}
+			rowToStruct(r, &borehole.URLs)
+		}
+		if (flags & dbf.NOTE) != 0 {
+			q, err = assets.ReadString("pg/note/by_borehole_id.sql")
+			if err != nil {
+				return nil, err
+			}
+			r, err := pg.pool.Query(context.Background(), q, id)
+			if err != nil {
+				return nil, err
+			}
+			rowToStruct(r, &borehole.Notes)
+		}
+		if (flags & dbf.GEOJSON) != 0 {
+			geojson, err := pg.queryRow("pg/borehole/geojson.sql", id)
+			if err != nil {
+				return nil, err
+			}
+			if geojson["geojson"] != nil {
+				borehole.GeoJSON = geojson["geojson"]
+			}
+		}
+		if (flags & dbf.MINING_DISTRICTS) != 0 {
+			q, err = assets.ReadString("pg/mining_district/by_borehole_id.sql")
+			if err != nil {
+				return nil, err
+			}
+			r, err := pg.pool.Query(context.Background(), q, id)
+			if err != nil {
+				return nil, err
+			}
+			rowToStruct(r, &borehole.MiningDistricts)
+		}
+		if (flags & dbf.QUADRANGLES) != 0 {
+			q, err = assets.ReadString("pg/quadrangle/250k_by_borehole_id.sql")
+			if err != nil {
+				return nil, err
+			}
+			r, err := pg.pool.Query(context.Background(), q, id)
+			if err != nil {
+				return nil, err
+			}
+			rowToStruct(r, &borehole.Quadrangles)
+		}
+		return &borehole, nil
 	}
-	if (flags & dbf.INVENTORY_SUMMARY) != 0 {
-		q, err = assets.ReadString("pg/keyword/group_by_borehole_id.sql")
-		if err != nil {
-			return nil, err
-		}
-		r, err := pg.pool.Query(context.Background(), q, id, ((flags & dbf.PRIVATE) == 0))
-		if err != nil {
-			return nil, err
-		}
-		rowToStruct(r, &borehole.KeywordSummary)
-	}
-	if (flags & dbf.ORGANIZATION) != 0 {
-		q, err = assets.ReadString("pg/organization/by_borehole_id.sql")
-		if err != nil {
-			return nil, err
-		}
-		r, err := pg.pool.Query(context.Background(), q, id)
-		if err != nil {
-			return nil, err
-		}
-		rowToStruct(r, &borehole.Organizations)
-	}
-	if (flags & dbf.URLS) != 0 {
-		q, err = assets.ReadString("pg/url/by_borehole_id.sql")
-		if err != nil {
-			return nil, err
-		}
-		r, err := pg.pool.Query(context.Background(), q, id)
-		if err != nil {
-			return nil, err
-		}
-		rowToStruct(r, &borehole.URLs)
-	}
-	if (flags & dbf.NOTE) != 0 {
-		q, err = assets.ReadString("pg/note/by_borehole_id.sql")
-		if err != nil {
-			return nil, err
-		}
-		r, err := pg.pool.Query(context.Background(), q, id)
-		if err != nil {
-			return nil, err
-		}
-		rowToStruct(r, &borehole.Notes)
-	}
-	if (flags & dbf.GEOJSON) != 0 {
-		geojson, err := pg.queryRow("pg/borehole/geojson.sql", id)
-		if err != nil {
-			return nil, err
-		}
-		if geojson["geojson"] != nil {
-			borehole.GeoJSON = geojson["geojson"]
-		}
-	}
-	if (flags & dbf.MINING_DISTRICTS) != 0 {
-		q, err = assets.ReadString("pg/mining_district/by_borehole_id.sql")
-		if err != nil {
-			return nil, err
-		}
-		r, err := pg.pool.Query(context.Background(), q, id)
-		if err != nil {
-			return nil, err
-		}
-		rowToStruct(r, &borehole.MiningDistricts)
-	}
-	if (flags & dbf.QUADRANGLES) != 0 {
-		q, err = assets.ReadString("pg/quadrangle/250k_by_borehole_id.sql")
-		if err != nil {
-			return nil, err
-		}
-		r, err := pg.pool.Query(context.Background(), q, id)
-		if err != nil {
-			return nil, err
-		}
-		rowToStruct(r, &borehole.Quadrangles)
-	}
-	return &borehole, nil
+	return nil, nil
 }
