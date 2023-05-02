26ff6dad09dc6b9e77c8ab983c4a5499641a262b
Tommy Folan <tommy.folan@alaska.gov>
2023-05-02 08:55:15 -0800

    Add progress bar for file uploads

--- assets/js/filedrop.js
+++ assets/js/filedrop.js
@@ -4,23 +4,36 @@ async function uploadFiles(files) {
         formData.append('files', files[i]);
     }
     try {
-        let response = await fetch('upload', {
-            method: 'POST',
-            body: formData
+        let xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener('progress', (event) => {
+            if (event.lengthComputable) {
+                let percentCompleted = Math.round((event.loaded / event.total) * 100);
+                document.querySelector('#progress-bar').style.width = percentCompleted + '%';
+            }
         });
+        xhr.addEventListener('load', (event) => {
+					if (xhr.status >= 200 && xhr.status < 300) {
+							document.querySelector('#progress-bar').textContent = 'Uploads succeeded';
+					} else {
+							document.querySelector('#progress-bar').textContent = 'An error occurred while uploading the files';
+							document.querySelector('#progress-bar').style.backgroundColor = 'Tomato';
+					}
+        });
+        xhr.open('POST', 'upload');
+        xhr.send(formData);
     } catch (error) {
-        console.error(error);
+        console.log(error)
     }
-	}
+}
 
 const dropZone = document.querySelector('#file-drop');
 
-dropZone.addEventListener('dragover', (e)=>{
-	e.preventDefault();
+dropZone.addEventListener('dragover', (e) => {
+    e.preventDefault();
 })
 
-dropZone.addEventListener('drop', (e)=>{
-	e.preventDefault();
-	var files = e.dataTransfer.files;
-	uploadFiles(files)
-})
+dropZone.addEventListener('drop', (e) => {
+    e.preventDefault();
+    var files = e.dataTransfer.files;
+    uploadFiles(files)
+});
