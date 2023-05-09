6c8caa9531e431a88438d137bb851c8f9958a6b5
Tommy Folan <tommy.folan@alaska.gov>
2023-05-09 11:46:30 -0800

    Modify progress bars to handle long filenames gracefully

--- assets/js/filedrop.js
+++ assets/js/filedrop.js
@@ -1,13 +1,47 @@
+const pbFile = document.querySelector('#pb-file');
+const pbFileName = document.querySelector('#pb-file-name');
+const pbTotalCount = document.querySelector('#pb-total-count');
+const dropZone = document.querySelector('#file-drop');
+const fileInput = document.querySelector('#file-input');
+const fileDropText = document.getElementById('file-drop-text');
+
+dropZone.addEventListener('dragover', (e) => {
+	e.preventDefault();
+})
+
+dropZone.addEventListener('drop', (e) => {
+	e.preventDefault();
+	pbFile.textContent = '';
+	pbFileName.textContent = '';
+	var files = e.dataTransfer.files;
+	pbTotalCount.textContent = 0 + ' / ' + files.length + " Files Transfered";
+	uploadFiles(files);
+	fileDropText.style.display = 'none';
+});
+
+dropZone.addEventListener('click', () => {
+	fileInput.click();
+});
+
+fileInput.addEventListener('change', () => {
+	let files = fileInput.files;
+	uploadFiles(files);
+	fileDropText.style.display = 'none';
+	count = +1
+	pbTotalCount.textContent = count + ' / ' + files.length + " Files Transfered";
+});
+
 async function uploadFiles(files) {
 	let totalSize = 0;
 	let totalLoaded = 0;
 	let count = 0;
-	document.querySelector('#progress-bar-file').style.display = 'block';
-	document.querySelector('#progress-bar-total').style.display = 'block';
-	document.querySelector('#progress-bar-file').style.backgroundColor = '#cfe4fa';
-	document.querySelector('#progress-bar-file-name').style.backgroundColor = '#cfe4fa';
-	document.querySelector('#progress-bar-total').style.backgroundColor = '#cfe4fa';
-	document.querySelector('#progress-bar-total-count').style.backgroundColor = '#cfe4fa';
+	const progressBarFile = document.querySelector('#progress-bar-file');
+	const progressBarTotal = document.querySelector('#progress-bar-total');
+	const pbTotal = document.querySelector('#pb-total');
+
+	progressBarFile.style.display = 'block';
+	progressBarTotal.style.display = 'block';
+	pbFileName.classList.remove('long-name');
 
 	for (let i = 0; i < files.length; i++) {
 		totalSize += files[i].size;
@@ -16,34 +50,29 @@ async function uploadFiles(files) {
 	for (let file of files) {
 		let formData = new FormData();
 		formData.append('file', file);
-
 		let xhr = new XMLHttpRequest();
 		xhr.upload.addEventListener('progress', (event) => {
 			if (event.lengthComputable) {
 				let percentCompletedFile = Math.round((event.loaded / event.total) * 100);
 				let percentCompletedTotal = Math.round(((totalLoaded + event.loaded) / totalSize) * 100);
 
-				document.querySelector('#progress-bar-file').style.width = percentCompletedFile + '%';
-				document.querySelector('#pb-file').textContent = formatSize(event.loaded) + ' / ' + formatSize(event.total);
+				progressBarFile.style.width = percentCompletedFile + '%';
+				pbFile.textContent = formatSize(event.loaded) + ' / ' + formatSize(event.total);
+				pbFileName.textContent = file.name;
 
-				document.querySelector('#progress-bar-file-name').style.width = percentCompletedFile + '%';
-				document.querySelector('#pb-file-name').textContent = file.name;
-
-				document.querySelector('#progress-bar-total').style.width =
-					percentCompletedTotal + '%';
-				console.log(formatSize(totalSize));
-
-				document.querySelector('#pb-total').textContent =
-					formatSize(totalLoaded + event.loaded) + ' / ' + formatSize(totalSize);
-				document.querySelector('#progress-bar-total-count').style.width =
-					percentCompletedTotal + '%';
+				if (pbFileName.scrollWidth > pbFileName.clientWidth) {
+					pbFileName.classList.add('long-name');
+				}
+				progressBarTotal.style.width = percentCompletedTotal + '%';
+				pbTotal.textContent = formatSize(totalLoaded + event.loaded) + ' / '
+				+ formatSize(totalSize);
 			}
 		});
 		xhr.addEventListener('load', (event) => {
 			if (xhr.status >= 200 && xhr.status < 300) {
 				totalLoaded += file.size;
 				count += 1
-				document.querySelector('#pb-total-count').textContent =
+				pbTotalCount.textContent =
 					count + ' / ' + files.length + " Files Transfered";
 			}
 		});
@@ -55,36 +84,6 @@ async function uploadFiles(files) {
 	}
 }
 
-const dropZone = document.querySelector('#file-drop');
-const filecount = document.querySelector('#file-count');
-const fileInput = document.querySelector('#file-input');
-
-dropZone.addEventListener('dragover', (e) => {
-	e.preventDefault();
-})
-
-dropZone.addEventListener('drop', (e) => {
-	e.preventDefault();
-	document.querySelector('#pb-file').textContent = '';
-	document.querySelector('#pb-file-name').textContent = '';
-	var files = e.dataTransfer.files;
-	document.querySelector('#pb-total-count').textContent = 0 + ' / ' + files.length + " Files Transfered";
-	uploadFiles(files);
-	document.getElementById('file-drop-text').style.display = 'none';
-});
-
-dropZone.addEventListener('click', () => {
-	fileInput.click();
-});
-
-fileInput.addEventListener('change', () => {
-	let files = fileInput.files;
-	uploadFiles(files);
-	document.getElementById('file-drop-text').style.display = 'none';
-	count = +1
-	document.querySelector('#pb-total-count').textContent =
-		count + ' / ' + files.length + " Files Transfered";
-});
 
 function formatSize(size) {
 	if (size < 1024) {
