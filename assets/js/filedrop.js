eedda547715ba44b5ec10086a68b093b8af9b72d
Tommy Folan <tommy.folan@alaska.gov>
2023-05-10 15:55:36 -0800

    Upload progress display is scaled by the size of the file

--- assets/js/filedrop.js
+++ assets/js/filedrop.js
@@ -35,13 +35,13 @@ async function uploadFiles(files) {
 	let totalSize = 0;
 	let totalLoaded = 0;
 	let count = 0;
+	
 	const progressBarFile = document.querySelector('#progress-bar-file');
 	const progressBarTotal = document.querySelector('#progress-bar-total');
 	const pbTotal = document.querySelector('#pb-total');
 
 	progressBarFile.style.display = 'flex';
 	progressBarTotal.style.display = 'flex';
-
 
 	for (let i = 0; i < files.length; i++) {
 		totalSize += files[i].size;
@@ -55,13 +55,12 @@ async function uploadFiles(files) {
 			if (event.lengthComputable) {
 				let percentCompletedFile = Math.round((event.loaded / event.total) * 100);
 				let percentCompletedTotal = Math.round(((totalLoaded + event.loaded) / totalSize) * 100);
-
 				progressBarFile.style.width = percentCompletedFile + '%';
-				pbFile.textContent = formatSize(event.loaded) + ' / ' + formatSize(event.total);
+				pbFile.textContent = formatSize(Math.round((event.loaded / event.total) * file.size), file.size) + ' / ' + formatSize(file.size, file.size);
 				pbFileName.textContent = file.name;
 				progressBarTotal.style.width = percentCompletedTotal + '%';
-				pbTotal.textContent = formatSize(totalLoaded + event.loaded) + ' / ' +
-					formatSize(totalSize);
+				pbTotal.textContent = formatSize(((totalLoaded + event.loaded) / event.total) * file.size, file.size) + ' / ' +
+					formatSize(totalSize, file.size);
 			}
 		});
 		xhr.addEventListener('load', (event) => {
@@ -73,6 +72,7 @@ async function uploadFiles(files) {
 			}
 		});
 		xhr.open('POST', 'upload');
+
 		await new Promise(resolve => {
 			xhr.addEventListener('load', resolve);
 			xhr.send(formData);
@@ -80,13 +80,12 @@ async function uploadFiles(files) {
 	}
 }
 
-
-function formatSize(size) {
-	if (size < 1024) {
+function formatSize(size, fileSize) {
+	if (fileSize < 1024) {
 		return size + ' B';
-	} else if (size < 1024 * 1024) {
-		return (size / 1024).toFixed(2) + ' KB';
-	} else if (size < 1024 * 1024 * 1024) {
+	} else if (fileSize < 1024 * 1024) {
+		return (size / 1024).toFixed(0) + ' KB';
+	} else if (fileSize < 1024 * 1024 * 1024) {
 		return (size / (1024 * 1024)).toFixed(2) + ' MB';
 	} else {
 		return (size / (1024 * 1024 * 1024)).toFixed(2) + ' GB';
