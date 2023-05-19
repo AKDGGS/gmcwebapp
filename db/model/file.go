60490d136f500e40fd78f93e8ac68eee214dbd2d
Tommy Folan <tommy.folan@alaska.gov>
2023-05-19 09:21:10 -0800

    Multiply out 1024 * 1024

--- db/model/file.go
+++ db/model/file.go
@@ -9,15 +9,16 @@ type File struct {
 	Size        int64  `json:"size"`
 	Type        string `json:"mimetype"`
 	MD5         string `json:"content_md5"`
+	BorholeIds  []int  `json:"borehole_ids"`
 	WellIDs     []int  `json:"well_ids"`
 }
 
 func (f *File) FormattedSize() string {
 	if f.Size < 1024 {
 		return fmt.Sprintf("%d B", f.Size)
-	} else if f.Size < 1024*1024 {
+	} else if f.Size < 1048576 {
 		return fmt.Sprintf("%.1f KB", float64(f.Size)/1024)
 	} else {
-		return fmt.Sprintf("%.1f MB", float64(f.Size)/(1024*1024))
+		return fmt.Sprintf("%.1f MB", float64(f.Size)/(1048576))
 	}
 }
