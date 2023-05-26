1d483a9294da8fdd0d51955aed48d81883d8bcd5
Tommy Folan <tommy.folan@alaska.gov>
2023-05-26 14:09:00 -0800

    Clean up code
    Issues: file size is not correct and no way to select files after an error other than dropping them.

--- assets/js/filedrop.js
+++ assets/js/filedrop.js
@@ -1,16 +1,20 @@
-const drop_zone = document.getElementById('file-drop');
-const file_input = document.getElementById('file-input');
+const drop_zone = document.getElementById('filedrop');
+const file_input = document.getElementById('filedrop-file-input');
 
-const upload_link = document.getElementById('upload-link');
+const upload_link = document.getElementById('filedrop-upload-link');
 
-const pb_file = document.querySelector('.pb-file');
-const pb_file_progress = document.getElementById('pb-file-progress');
-const pb_file_name = document.getElementById('pb-file-name');
+const pb_file = document.querySelector('.filedrop-pb-file');
+const pb_file_progress = document.getElementById('filedrop-pb-file-progress');
+const pb_file_name = document.getElementById('filedrop-pb-file-name');
 
-const pb_total = document.querySelector('.pb-total');
-const pb_total_progress = document.getElementById('pb-total-progress');
-const pb_total_count = document.getElementById('pb-total-count');
+const pb_total = document.querySelector('.filedrop-pb-total');
+const pb_total_progress = document.getElementById('filedrop-pb-total-progress');
+const pb_total_count = document.getElementById('filedrop-pb-total-count');
+
+let uploading = false;
+let all_files = [];
 let uploaded_files = [];
+let count = 0;
 
 drop_zone.addEventListener('dragover', (e) => {
 	e.preventDefault();
@@ -18,7 +22,13 @@ drop_zone.addEventListener('dragover', (e) => {
 
 drop_zone.addEventListener('drop', (e) => {
 	e.preventDefault();
-	let error_div = document.querySelector('.error-div');
+	if (uploading) {
+		return;
+	}
+	uploading = true;
+	const uploadText = document.getElementById('filedrop-upload-text');
+	uploadText.classList.add('uploading');
+	let error_div = document.querySelector('.filedrop-error-div');
 	if (error_div) {
 		error_div.style.display = 'none';
 		error_div.failed_files = [];
@@ -28,23 +38,41 @@ drop_zone.addEventListener('drop', (e) => {
 		}
 	}
 
-	all_files = [];
-	uploaded_files = [];
-
-	var files = e.dataTransfer.files;
-	pb_total_count.textContent = 0 + ' / ' + files.length + " Files Transfered";
+	let files = e.dataTransfer.files;
+	pb_total_count.textContent = count + ' / ' + files.length + " Files Transfered";
 	upload_files(files, () => {
-		console.log('All files uploaded');
+		const uploadText = document.getElementById('filedrop-upload-text');
+		uploadText.classList.remove('uploading');
+		uploading = false;
 	});
 });
 
-drop_zone.addEventListener('click', () => {
+upload_link.addEventListener('click', (e) => {
+	e.preventDefault();
+	if (uploading) {
+		console.log("uploading true");
+		return
+	}
+	let error_div = document.querySelector('.filedrop-error-div');
+	if (error_div) {
+		error_div.style.display = 'none';
+		error_div.failed_files = [];
+		let le = error_div.querySelector('ul');
+		if (le) {
+			error_div.removeChild(le);
+		}
+	}
 	file_input.click();
 });
 
 file_input.addEventListener('change', () => {
 	let files = file_input.files;
-	count = +1
+	count += 1;
+	upload_files(files, () => {
+		const uploadText = document.getElementById('filedrop-upload-text');
+		uploadText.classList.remove('uploading');
+		uploading = false;
+	});
 	pb_total_count.textContent = count + ' / ' + files.length + " Files Transfered";
 });
 
@@ -52,7 +80,7 @@ function upload_files(files, uploaded_check) {
 	let file_count = 0;
 	let total_size = 0;
 	let total_loaded = 0;
-	let count = 0;
+	count = 0;
 
 	pb_file.style.display = 'flex';
 	pb_total.style.display = 'flex';
@@ -83,17 +111,17 @@ function upload_files(files, uploaded_check) {
 				let percent_completed_file = Math.round((event.loaded / event.total) * 100);
 				let percent_completed_total = Math.round(((total_loaded + event.loaded) / total_size) * 100);
 				pb_file.style.width = percent_completed_file + '%';
-				pb_file_progress.textContent = format_size(Math.round((event.loaded / event.total)
-					* file.size), file.size) + ' / ' + format_size(file.size, file.size);
+				pb_file_progress.textContent = format_size(Math.round((event.loaded / event.total) *
+					file.size), file.size) + ' / ' + format_size(file.size, file.size);
 				pb_file_name.textContent = file.name;
 				pb_total.style.width = percent_completed_total + '%';
-				pb_total_progress.textContent = format_size((total_loaded + event.loaded),
+				pb_total_progress.textContent = format_size((total_loaded + event.loaded) / event.total * total_size,
 					total_size) + ' / ' + format_size(total_size, total_size);
 			}
 		});
 
 		xhr.addEventListener('load', (event) => {
-			if (xhr.status >= 200 && xhr.status < 300) {
+			if (xhr.status == 200) {
 				total_loaded += file.size;
 				count += 1
 				pb_total_count.textContent =
@@ -131,7 +159,7 @@ function format_size(uploaded_amt, unit_size) {
 
 function add_file_to_page(file) {
 	const file_div = document.createElement('div');
-	const drop_zone = document.getElementById('file-drop');
+	const drop_zone = document.getElementById('filedrop');
 	const file_id = drop_zone.dataset.file_id;
 	const file_link = document.createElement('a');
 	file_link.href = `../file/${file_id}/${file.name}`;
@@ -146,14 +174,14 @@ function add_file_to_page(file) {
 	const file_size = document.createTextNode(` (${f_size})`);
 	file_div.appendChild(file_size);
 
-	const container = document.querySelector('.container');
-	const progress_bar = container.querySelector('.pb-total');
+	const container = document.querySelector('.filedrop-container');
+	const progress_bar = container.querySelector('.filedrop-pb-total');
 	container.insertBefore(file_div, progress_bar.nextSibling);
 }
 
 function upload_error() {
 	uploading = false;
-	let error_div = document.querySelector('.error-div');
+	let error_div = document.querySelector('.filedrop-error-div');
 	error_div.style.display = 'flex';
 
 	let failed_files = all_files.filter(file => !uploaded_files.includes(file));
