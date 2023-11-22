ffd1676580b4513053112285f834f640b1930bc0
Tommy Folan <tommy.folan@alaska.gov>
2023-11-22 09:03:15 -0900

    Remove unnecessary transaction code
    Simple inserts are automatically wrapped in a transaction.

--- db/pg/add_container.go
+++ db/pg/add_container.go
@@ -12,21 +12,12 @@ func (pg *Postgres) AddContainer(barcode string, name string, remark string) err
 	if barcode == "" || len(strings.TrimSpace(barcode)) < 1 {
 		return errors.New("Barcode cannot be empty")
 	}
-	tx, err := pg.pool.Begin(context.Background())
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback(context.Background())
 	q, err := assets.ReadString("pg/container/insert.sql")
 	if err != nil {
 		return err
 	}
-	_, err = tx.Exec(context.Background(), q, barcode, name, remark)
+	_, err = pg.pool.Exec(context.Background(), q, barcode, name, remark)
 	if err != nil {
-		return err
-	}
-	// If the insert is successful, commit the changes
-	if err := tx.Commit(context.Background()); err != nil {
 		return err
 	}
 	return nil
--- db/pg/add_inventory_quality.go
+++ db/pg/add_inventory_quality.go
@@ -12,17 +12,12 @@ func (pg *Postgres) AddInventoryQuality(barcode string, remark string, issues []
 	if barcode == "" || len(strings.TrimSpace(barcode)) < 1 {
 		return errors.New("Barcode cannot be empty")
 	}
-	tx, err := pg.pool.Begin(context.Background())
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback(context.Background())
 	q, err := assets.ReadString("pg/inventory/get_inventory_ids_by_barcode.sql")
 	if err != nil {
 		return err
 	}
 	var inventory_id int32
-	err = tx.QueryRow(context.Background(), q, barcode).Scan(&inventory_id)
+	err = pg.pool.QueryRow(context.Background(), q, barcode).Scan(&inventory_id)
 	if err != nil {
 		return err
 	}
@@ -30,12 +25,8 @@ func (pg *Postgres) AddInventoryQuality(barcode string, remark string, issues []
 	if err != nil {
 		return err
 	}
-	_, err = tx.Exec(context.Background(), q, inventory_id, remark, username, issues)
+	_, err = pg.pool.Exec(context.Background(), q, inventory_id, remark, username, issues)
 	if err != nil {
-		return err
-	}
-	// If the insert is successful, commit the changes
-	if err := tx.Commit(context.Background()); err != nil {
 		return err
 	}
 	return nil
