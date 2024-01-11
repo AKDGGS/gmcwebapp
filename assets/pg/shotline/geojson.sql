1122806bd194b3548e24705882076044020bb327
Tommy Folan <tommy.folan@alaska.gov>
2024-01-11 13:49:58 -0900

    Add nearby shotlines to popup

--- assets/pg/shotline/geojson.sql
+++ assets/pg/shotline/geojson.sql
@@ -16,7 +16,35 @@ SELECT jsonb_build_object(
 				'year', q.year,
 				'remark', q.remark,
 				'shotpoint_min', q.shotpoint_min,
-				'shotpoint_max', q.shotpoint_max
+				'shotpoint_max', q.shotpoint_max,
+			'nearby_shotlines', (
+				SELECT jsonb_agg(jsonb_build_object(
+					'shotline_id', nearby_shotlines.shotline_id,
+					'name', nearby_shotlines.name,
+					'distance', nearby_shotlines.distance
+				))
+				FROM (
+					SELECT sl.shotline_id, sl.name,
+					ROUND((ST_Distance(q.geog, sl.geog)/ 1609.344):: numeric, 2) AS distance
+					FROM (
+						SELECT ST_Makeline(
+							p.geog::geometry ORDER BY sp.shotpoint_number DESC
+						) AS geog, sl.shotline_id, sl.name
+						FROM shotline AS sl
+						JOIN shotpoint AS sp
+							ON sp.shotline_id = sl.shotline_id
+						JOIN shotpoint_point AS spp
+							ON spp.shotpoint_id = sp.shotpoint_id
+						JOIN point AS p
+							ON p.point_id = spp.point_id
+						WHERE ST_DWithin(q.geog, p.geog, 2414.016)
+						GROUP BY sl.shotline_id
+					) AS sl
+					WHERE q.shotline_id != sl.shotline_id AND ST_DWithin(q.geog, sl.geog, 2414.016)
+					ORDER BY distance
+					LIMIT 10
+				) AS nearby_shotlines
+				)
 			))
 		)
 	)) AS geojson
