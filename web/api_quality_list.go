585bbd99fdef8d1c8a4a447d2ca73167ebc78443
Tommy Folan <tommy.folan@alaska.gov>
2024-01-24 07:24:00 -0900

    Remove user verification and token check

--- web/api_quality_list.go
+++ web/api_quality_list.go
@@ -7,18 +7,6 @@ import (
 )
 
 func (srv *Server) ServeAPIQualityList(w http.ResponseWriter, r *http.Request) {
-	user, err := srv.Auths.CheckRequest(w, r)
-	if err != nil {
-		http.Error(
-			w, fmt.Sprintf("Authentication error: %s", err.Error()),
-			http.StatusBadRequest,
-		)
-		return
-	}
-	if user == nil {
-		http.Error(w, "Access denied", http.StatusForbidden)
-		return
-	}
 	list, err := srv.DB.ListIssues()
 	if err != nil {
 		http.Error(
