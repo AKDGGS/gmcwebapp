34ec142ae0cb57a5ac84d96d7266b29b11126961
Tommy Folan <tommy.folan@alaska.gov>
2024-01-31 15:03:42 -0900

    Correct labeling of transient fields

--- db/model/prospect.go
+++ db/model/prospect.go
@@ -1,15 +1,15 @@
 package model
 
 type Prospect struct {
-	ID         int32   `json:"id,omitempty"`
-	Name       string  `json:"name,omitempty"`
-	AltNames   *string `json:"alt_names,omitempty"`
-	ARDFNumber *string `json:"ardf_number,omitempty"`
+	ID         int32      `json:"id,omitempty"`
+	Name       string     `json:"name,omitempty"`
+	AltNames   *string    `json:"alt_names,omitempty"`
+	ARDFNumber *string    `json:"ardf_number,omitempty"`
+	Boreholes  []Borehole `json:"boreholes,omitempty"`
+	Files      []File     `json:"files,omitempty"`
 
 	//transient fields that are generated on-the-fly
 	//these fields don't exist in the database
-	Boreholes       []Borehole       `json:"boreholes,omitempty"`
-	Files           []File           `json:"files,omitempty"`
 	KeywordSummary  []KeywordSummary `json:"keywords,omitempty"`
 	GeoJSON         interface{}      `json:"geojson,omitempty"`
 	MiningDistricts []MiningDistrict `json:"mining_districts,omitempty"`
