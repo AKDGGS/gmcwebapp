afc5b8b7a2663d44f6f46ebb3c757e16ecbdf861
Tommy Folan <tommy.folan@alaska.gov>
2024-06-21 09:15:58 -0800

    Remove unnessary response check

--- assets/js/wells.js
+++ assets/js/wells.js
@@ -61,10 +61,6 @@ fetch('points.json')
 	.then(response => {
 		if (!response.ok) throw new Error(response.status + " " +
 			response.statusText);
-		if (!response) {
-			console.error('No response received')
-			return null;
-		}
 		return response.json();
 	})
 	.then(d => {
