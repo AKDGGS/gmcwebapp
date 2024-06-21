af099a6a3f9a66118b5ca5d93ea15211ab4db9e8
Tommy Folan <tommy.folan@alaska.gov>
2024-06-21 07:54:28 -0800

    Update GeoJSON queries to handle the absence of results

--- assets/pg/outcrop/geojson.sql
+++ assets/pg/outcrop/geojson.sql
@@ -1,75 +1,83 @@
 SELECT jsonb_build_object(
 	'type', 'FeatureCollection',
-	'features', jsonb_agg( jsonb_build_object(
+	'features', q.features
+) AS geojson
+FROM (
+	SELECT jsonb_agg(jsonb_build_object(
 		'type', 'Feature',
-		'geometry', ST_AsGeoJSON(o1.geog, 5, 0):: jsonb,
+		'geometry', ST_AsGeoJSON(geometry, 5, 0)::jsonb,
 		'properties', jsonb_strip_nulls(jsonb_build_object(
-			'outcrop_id', o1.outcrop_id,
-			'name', o1.name,
+			'outcrop_id', outcrop_id,
+			'name', name,
 			'nearby_outcrops', (
 				SELECT jsonb_agg(jsonb_build_object(
 					'outcrop_id', nearby_outcrops.outcrop_id,
 					'name', nearby_outcrops.name,
-					'distance',	nearby_outcrops.distance
+					'distance', nearby_outcrops.distance
 				))
 				FROM (
 					SELECT
-					o2.outcrop_id, o2.name,
-					ROUND((ST_Distance(o1.geog, o2.geog)/ 1609.344):: numeric, 2) AS distance
+						o1_copy.outcrop_id, o1_copy.name,
+						ROUND((ST_Distance(geometry, o1_copy.geog) / 1609.344)::numeric, 2) AS distance
 					FROM (
-						SELECT o1.outcrop_id, o1.name, p1.geog
-						FROM outcrop AS o1
-						JOIN outcrop_point AS op1 ON op1.outcrop_id = o1.outcrop_id
+						SELECT o1_copy.outcrop_id, o1_copy.name, p1.geog
+						FROM outcrop AS o1_copy
+						JOIN outcrop_point AS op1 ON op1.outcrop_id = o1_copy.outcrop_id
 						JOIN point AS p1 ON p1.point_id = op1.point_id
+						WHERE combined_data.outcrop_id != o1_copy.outcrop_id AND ST_DWithin(geometry, p1.geog, 2414.016)
+					) AS o1_copy
 
-						UNION ALL
+					UNION ALL
 
-						SELECT o1.outcrop_id, o1.name, place.geog
-						FROM outcrop AS o1
-						JOIN outcrop_place AS oplace ON oplace.outcrop_id = o1.outcrop_id
+					SELECT
+						o2.outcrop_id, o2.name,
+						ROUND((ST_Distance(geometry, o2.geog) / 1609.344)::numeric, 2) AS distance
+					FROM (
+						SELECT o2.outcrop_id, o2.name, place.geog
+						FROM outcrop AS o2
+						JOIN outcrop_place AS oplace ON oplace.outcrop_id = o2.outcrop_id
 						JOIN place ON place.place_id = oplace.place_id
+						WHERE combined_data.outcrop_id != o2.outcrop_id AND ST_DWithin(geometry, place.geog, 2414.016)
 					) AS o2
-					WHERE o1.outcrop_id != o2.outcrop_id AND ST_DWithin(o1.geog, o2.geog, 2414.016)
 					ORDER BY distance
-					LIMIT 10)
-					AS nearby_outcrops
-				))
-			))
-		)
-	) AS geojson
-FROM (
-	SELECT o1.outcrop_id, o1.name, p1.geog
-	FROM outcrop AS o1
-	JOIN outcrop_point AS op1 ON op1.outcrop_id = o1.outcrop_id
-	JOIN point AS p1 ON p1.point_id = op1.point_id
+					LIMIT 10
+				) AS nearby_outcrops
+			)
+		))
+	)) AS features
+	FROM (
+		SELECT p1.geog AS geometry, o1.outcrop_id, o1.name
+		FROM outcrop AS o1
+		JOIN outcrop_point AS op1 ON op1.outcrop_id = o1.outcrop_id
+		JOIN point AS p1 ON p1.point_id = op1.point_id
 
-	UNION ALL
+		UNION ALL
 
-	SELECT
-	o1.outcrop_id, o1.name, plss.geog
-	FROM outcrop AS o1
-	JOIN outcrop_plss AS oplss ON oplss.outcrop_id = o1.outcrop_id
-	JOIN plss ON plss.plss_id = oplss.plss_id
+		SELECT plss.geog AS geometry, o1.outcrop_id, o1.name
+		FROM outcrop AS o1
+		JOIN outcrop_plss AS oplss ON oplss.outcrop_id = o1.outcrop_id
+		JOIN plss ON plss.plss_id = oplss.plss_id
 
-	UNION ALL
+		UNION ALL
 
-	SELECT o1.outcrop_id, o1.name, place.geog
-	FROM outcrop AS o1
-	JOIN outcrop_place AS oplace ON oplace.outcrop_id = o1.outcrop_id
-	JOIN place ON place.place_id = oplace.place_id
+		SELECT place.geog AS geomerty, o1.outcrop_id, o1.name
+		FROM outcrop AS o1
+		JOIN outcrop_place AS oplace ON oplace.outcrop_id = o1.outcrop_id
+		JOIN place ON place.place_id = oplace.place_id
 
-	UNION ALL
+		UNION ALL
 
-	SELECT o1.outcrop_id, o1.name, region.geog
-	FROM outcrop AS o1
-	JOIN outcrop_region AS oregion ON oregion.outcrop_id = o1.outcrop_id
-	JOIN region ON region.region_id = oregion.region_id
+		SELECT region.geog AS geometry, o1.outcrop_id, o1.name
+		FROM outcrop AS o1
+		JOIN outcrop_region AS oregion ON oregion.outcrop_id = o1.outcrop_id
+		JOIN region ON region.region_id = oregion.region_id
+		UNION ALL
 
-	UNION ALL
-
-	SELECT o1.outcrop_id, o1.name, quadrangle.geog
-	FROM outcrop AS o1
-	JOIN outcrop_quadrangle AS oquad ON oquad.outcrop_id = o1.outcrop_id
-	JOIN quadrangle ON quadrangle.quadrangle_id = oquad.quadrangle_id
-) AS o1
-WHERE o1.outcrop_id = $1 AND o1.geog IS NOT NULL;
+		SELECT quadrangle.geog AS geometry, o1.outcrop_id, o1.name
+		FROM outcrop AS o1
+		JOIN outcrop_quadrangle AS oquad ON oquad.outcrop_id = o1.outcrop_id
+		JOIN quadrangle ON quadrangle.quadrangle_id = oquad.quadrangle_id
+	) AS combined_data
+	WHERE combined_data.outcrop_id = $1 AND geometry IS NOT NULL
+) AS q
+WHERE q.features IS NOT NULL;
--- assets/pg/shotline/geojson.sql
+++ assets/pg/shotline/geojson.sql
@@ -1,22 +1,23 @@
 SELECT jsonb_build_object(
 	'type', 'FeatureCollection',
-	'features', jsonb_build_array(
-		jsonb_build_object(
-			'type', 'Feature',
-			'geometry',
-			ST_AsGeoJSON(
-				CASE
-					WHEN ST_NumPoints(q.geog) > 1 THEN ST_Simplify(q.geog,0.0001)
-					ELSE ST_PointN(q.geog, 1)
-				END, 5, 0)::jsonb,
-			'properties', jsonb_strip_nulls(jsonb_build_object(
-				'shotline_id', q.shotline_id,
-				'name', q.name,
-				'alt_names', q.alt_names,
-				'year', q.year,
-				'remark', q.remark,
-				'shotpoint_min', q.shotpoint_min,
-				'shotpoint_max', q.shotpoint_max,
+	'features', subquery.features
+) AS geojson
+FROM (
+	SELECT jsonb_agg(jsonb_build_object(
+		'type', 'Feature',
+		'geometry', ST_AsGeoJSON(
+			CASE
+			WHEN ST_NumPoints(q.geog) > 1 THEN ST_Simplify(q.geog, 0.0001)
+			ELSE ST_PointN(q.geog, 1)
+			END, 5, 0)::jsonb,
+		'properties', jsonb_build_object(
+			'shotline_id', q.shotline_id,
+			'name', q.name,
+			'alt_names', q.alt_names,
+			'year', q.year,
+			'remark', q.remark,
+			'shotpoint_min', q.shotpoint_min,
+			'shotpoint_max', q.shotpoint_max,
 			'nearby_shotlines', (
 				SELECT jsonb_agg(jsonb_build_object(
 					'shotline_id', nearby_shotlines.shotline_id,
@@ -44,29 +45,27 @@ SELECT jsonb_build_object(
 					ORDER BY distance
 					LIMIT 10
 				) AS nearby_shotlines
-				)
-			))
+			)
 		)
-	)) AS geojson
-FROM (
-	SELECT ST_Makeline(
-		p.geog::geometry ORDER BY sp.shotpoint_number DESC
-	) AS geog,
-		sl.shotline_id AS shotline_id,
-		sl.name AS name,
-		sl.alt_names as alt_names,
-		sl.year as year,
-		sl.remark as remark,
-		MIN(sp.shotpoint_number) AS shotpoint_min,
-		MAX(sp.shotpoint_number) AS shotpoint_max
-	FROM shotline AS sl
-	JOIN shotpoint AS sp
-		ON sp.shotline_id = sl.shotline_id
-	JOIN shotpoint_point AS spp
-		ON spp.shotpoint_id = sp.shotpoint_id
-	JOIN point AS p
-		ON p.point_id = spp.point_id
-	WHERE sl.shotline_id = $1
-	GROUP BY sl.shotline_id, sl.name
-) AS q
-WHERE q.geog IS NOT NULL
+	)) AS features
+	FROM (
+		SELECT
+			ST_Makeline(p.geog::geometry ORDER BY sp.shotpoint_number DESC) AS geog,
+			ST_Length(ST_Makeline(p.geog::geometry ORDER BY sp.shotpoint_number DESC)) AS line_length,
+			sl.shotline_id,
+			sl.name,
+			sl.alt_names,
+			sl.year,
+			sl.remark,
+			MIN(sp.shotpoint_number) AS shotpoint_min,
+			MAX(sp.shotpoint_number) AS shotpoint_max
+		FROM shotline AS sl
+		JOIN shotpoint AS sp ON sp.shotline_id = sl.shotline_id
+		JOIN shotpoint_point AS spp ON spp.shotpoint_id = sp.shotpoint_id
+		JOIN point AS p ON p.point_id = spp.point_id
+		WHERE sl.shotline_id = $1
+		GROUP BY sl.shotline_id, sl.name, sl.alt_names, sl.year, sl.remark
+	) AS q
+	WHERE q.line_length > 0
+) AS subquery
+WHERE features IS NOT NULL
