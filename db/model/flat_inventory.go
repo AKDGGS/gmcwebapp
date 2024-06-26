cbf6e4f589038215239f334769e89acd5d9a4aec
Hugh Winston <hugh.winston@alaska.gov>
2024-06-25 17:51:02 -0800

    go fmt

--- db/model/flat_inventory.go
+++ db/model/flat_inventory.go
@@ -21,34 +21,34 @@ type FlatInventory struct {
 	Remark         *string         `json:"remark,omitempty"`
 	Geometries     json.RawMessage `json:"geometries,omitempty"`
 	CanPublish     bool            `db:"can_publish" json:"can_publish"`
-	Well          []struct {
+	Well           []struct {
 		ID       int32   `json:"id"`
 		Name     string  `json:"name"`
 		AltNames *string `json:"altnames,omitempty"`
 		Number   *string `json:"number,omitempty"`
 		API      *string `json:"api,omitempty"`
 	} `json:"well,omitempty"`
-	Outcrop		   []struct {
-		ID       int32   `json:"id"`
-		Name     string  `json:"name"`
-		Number   *string `json:"number,omitempty"`
-		Year     *int32 `json:"year,omitempty"`
+	Outcrop []struct {
+		ID     int32   `json:"id"`
+		Name   string  `json:"name"`
+		Number *string `json:"number,omitempty"`
+		Year   *int32  `json:"year,omitempty"`
 	} `json:"outcrop,omitempty"`
-	Borehole		[]struct {
-		ID       int32   `json:"id"`
-		Name     string  `json:"name"`
+	Borehole []struct {
+		ID       int32  `json:"id"`
+		Name     string `json:"name"`
 		Prospect struct {
-			ID       int32   `json:"id"`
-			Name     string  `json:"name"`
-			ARDF	 string  `json:"ardf"`
+			ID   int32  `json:"id"`
+			Name string `json:"name"`
+			ARDF string `json:"ardf"`
 		} `json:"prospect,omitempty"`
 	} `json:"borehole,omitempty"`
-	Shotlines		[]struct {
-		ID       int32    `json:"id"`
-		Name     string   `json:"name"`
-		Year     *int32   `json:"year,omitempty"`
-		Min      *float64 `json:"min,omitempty"`
-		Max      *float64 `json:"max,omitempty"`
+	Shotlines []struct {
+		ID   int32    `json:"id"`
+		Name string   `json:"name"`
+		Year *int32   `json:"year,omitempty"`
+		Min  *float64 `json:"min,omitempty"`
+		Max  *float64 `json:"max,omitempty"`
 	} `json:"shotlines,omitempty"`
 }
 
