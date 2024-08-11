05e3c1dc824136823f91448469998ba38e1184b3
Christopher Ramey <christopher.ramey@alaska.gov>
2024-08-11 14:18:56 -0800

    QA query optimization

--- assets/pg/qa/note_no_links.sql
+++ assets/pg/qa/note_no_links.sql
@@ -1,26 +1,40 @@
-SELECT note_id AS "Note ID",
-	username AS "User",
-	TO_CHAR(note_date, 'MM/dd/yyyy') AS "Date"
-FROM note
-WHERE note_id NOT IN (
-	SELECT DISTINCT note_id
+WITH ids AS (
+	SELECT note_id
 	FROM borehole_note
-) AND note_id NOT IN (
-	SELECT DISTINCT note_id
+
+	UNION
+
+	SELECT note_id
 	FROM inventory_note
-) AND note_id NOT IN (
-	SELECT DISTINCT note_id
+
+	UNION
+
+	SELECT note_id
 	FROM outcrop_note
-) AND note_id NOT IN (
-	SELECT DISTINCT note_id
+
+	UNION
+
+	SELECT note_id
 	FROM publication_note
-) AND note_id NOT IN (
-	SELECT DISTINCT note_id
+
+	UNION
+
+	SELECT note_id
 	FROM shotline_note
-) AND note_id NOT IN (
-	SELECT DISTINCT note_id
+
+	UNION
+
+	SELECT note_id
 	FROM well_note
-) AND note_id NOT IN (
-	SELECT DISTINCT note_id
+
+	UNION
+
+	SELECT note_id
 	FROM sample_note
 )
+SELECT n.note_id AS "Note ID",
+	username AS "User",
+	TO_CHAR(note_date, 'MM/dd/yyyy') AS "Date"
+FROM note AS n
+LEFT JOIN ids ON ids.note_id = n.note_id
+WHERE ids.note_id IS NULL
--- assets/pg/qa/outcrop_no_inventory.sql
+++ assets/pg/qa/outcrop_no_inventory.sql
@@ -1,11 +1,12 @@
-SELECT outcrop_id AS "Outcrop ID",
-	name AS "Outcrop Name"
-FROM outcrop AS o
-WHERE outcrop_id NOT IN (
+WITH ids AS (
 	SELECT DISTINCT io.outcrop_id
 	FROM inventory_outcrop AS io
 	JOIN inventory AS iv
 		ON iv.inventory_id = io.inventory_id
 	WHERE iv.active
 )
-ORDER BY outcrop_id ASC
+SELECT o.outcrop_id AS "Outcrop ID",
+	o.name AS "Outcrop Name"
+FROM outcrop AS o
+LEFT JOIN ids ON ids.outcrop_id = o.outcrop_id
+WHERE ids.outcrop_id IS NULL
