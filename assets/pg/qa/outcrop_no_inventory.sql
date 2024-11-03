89563786bc30136c7157f0b10f8a10418ca8da78
Christopher Ramey <christopher.ramey@alaska.gov>
2024-11-03 04:56:49 -0900

    Optimize outcrops without inventory QA query

--- assets/pg/qa/outcrop_no_inventory.sql
+++ assets/pg/qa/outcrop_no_inventory.sql
@@ -1,12 +1,9 @@
-WITH ids AS (
+SELECT outcrop_id AS "Outcrop ID", name AS "Outcrop Name"
+FROM outcrop
+WHERE outcrop_id NOT IN (
 	SELECT DISTINCT io.outcrop_id
 	FROM inventory_outcrop AS io
-	JOIN inventory AS iv
-		ON iv.inventory_id = io.inventory_id
-	WHERE iv.active
+	JOIN inventory AS i
+		ON i.active AND i.inventory_id = io.inventory_id
 )
-SELECT o.outcrop_id AS "Outcrop ID",
-	o.name AS "Outcrop Name"
-FROM outcrop AS o
-LEFT JOIN ids ON ids.outcrop_id = o.outcrop_id
-WHERE ids.outcrop_id IS NULL
+ORDER BY LOWER(name)
