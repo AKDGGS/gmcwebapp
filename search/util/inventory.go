906c9ee41e5cbebc16f79d3e5f50eb3f4c350dc6
Christopher Ramey <christopher.ramey@alaska.gov>
2024-11-09 09:05:50 -0900

    Move inventory search param parsing into struct method

--- search/util/inventory.go
+++ search/util/inventory.go
@@ -2,6 +2,9 @@ package util
 
 import (
 	"encoding/json"
+	"net/url"
+	"strconv"
+	"strings"
 	"time"
 
 	"gmc/db/model"
@@ -26,6 +29,73 @@ type InventoryParams struct {
 	Size           int
 	Private        bool
 	Sort           [][2]string
+}
+
+func (ip *InventoryParams) ParseQuery(q url.Values) {
+	var err error
+	ip.Size, err = strconv.Atoi(q.Get("size"))
+	if err != nil {
+		ip.Size = 25
+	}
+
+	ip.From, err = strconv.Atoi(q.Get("from"))
+	if err != nil || ip.From < 0 {
+		ip.From = 0
+	}
+
+	ip.Query = q.Get("q")
+
+	if t := q.Get("top"); t != "" {
+		if n, err := strconv.ParseFloat(t, 64); err == nil {
+			ip.IntervalTop = &n
+		}
+	}
+
+	if t := q.Get("bottom"); t != "" {
+		if n, err := strconv.ParseFloat(t, 64); err == nil {
+			ip.IntervalBottom = &n
+		}
+	}
+
+	// Save people from flipping top and bottom values
+	if ip.IntervalTop != nil && ip.IntervalBottom != nil {
+		if *ip.IntervalTop > *ip.IntervalBottom {
+			t := ip.IntervalTop
+			ip.IntervalTop = ip.IntervalBottom
+			ip.IntervalBottom = t
+		}
+	}
+
+	if keywords, ok := q["keyword"]; ok {
+		ip.Keywords = keywords
+	}
+
+	if ids, ok := q["prospect_id"]; ok {
+		for _, sid := range ids {
+			if id, err := strconv.Atoi(sid); err == nil {
+				ip.ProspectIDs = append(ip.ProspectIDs, id)
+			}
+		}
+	}
+
+	if ids, ok := q["collection_id"]; ok {
+		for _, sid := range ids {
+			if id, err := strconv.Atoi(sid); err == nil {
+				ip.CollectionIDs = append(ip.CollectionIDs, id)
+			}
+		}
+	}
+
+	if sorts, ok := q["sort"]; ok {
+		dirs, _ := q["dir"]
+		for i, v := range sorts {
+			dir := "asc"
+			if i < len(dirs) && strings.ToLower(dirs[i]) == "desc" {
+				dir = "desc"
+			}
+			ip.Sort = append(ip.Sort, [2]string{v, dir})
+		}
+	}
 }
 
 type InventoryResults struct {
--- web/search_inventory_json.go
+++ web/search_inventory_json.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
-	"strings"
 
 	sutil "gmc/search/util"
 )
@@ -21,77 +19,13 @@ func (srv *Server) ServeSearchInventoryJSON(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	q := r.URL.Query()
-
-	size, err := strconv.Atoi(q.Get("size"))
-	if err != nil {
-		size = 25
-	} else if user == nil && size > 1000 {
-		size = 25
-	} else if user != nil && size > 10000 {
-		size = 25
+	params := &sutil.InventoryParams{}
+	params.ParseQuery(r.URL.Query())
+	if (user == nil && params.Size > 1000) || (user != nil && params.Size > 10000) {
+		params.Size = 25
 	}
-
-	from, err := strconv.Atoi(q.Get("from"))
-	if err != nil || from < 0 {
-		from = 0
-	}
-
-	params := &sutil.InventoryParams{
-		Query:   q.Get("q"),
-		Size:    size,
-		From:    from,
-		Private: (user != nil),
-	}
-
-	if t := q.Get("top"); t != "" {
-		if n, err := strconv.ParseFloat(t, 64); err == nil {
-			params.IntervalTop = &n
-		}
-	}
-	if t := q.Get("bottom"); t != "" {
-		if n, err := strconv.ParseFloat(t, 64); err == nil {
-			params.IntervalBottom = &n
-		}
-	}
-	// Save people from flipping top and bottom values
-	if params.IntervalTop != nil && params.IntervalBottom != nil {
-		if *params.IntervalTop > *params.IntervalBottom {
-			t := params.IntervalTop
-			params.IntervalTop = params.IntervalBottom
-			params.IntervalBottom = t
-		}
-	}
-
-	if keywords, ok := q["keyword"]; ok {
-		params.Keywords = keywords
-	}
-
-	if ids, ok := q["prospect_id"]; ok {
-		for _, sid := range ids {
-			if id, err := strconv.Atoi(sid); err == nil {
-				params.ProspectIDs = append(params.ProspectIDs, id)
-			}
-		}
-	}
-
-	if ids, ok := q["collection_id"]; ok {
-		for _, sid := range ids {
-			if id, err := strconv.Atoi(sid); err == nil {
-				params.CollectionIDs = append(params.CollectionIDs, id)
-			}
-		}
-	}
-
-	if sorts, ok := q["sort"]; ok {
-		dirs, _ := q["dir"]
-		for i, v := range sorts {
-			dir := "asc"
-			if i < len(dirs) && strings.ToLower(dirs[i]) == "desc" {
-				dir = "desc"
-			}
-			params.Sort = append(params.Sort, [2]string{v, dir})
-		}
+	if user != nil {
+		params.Private = true
 	}
 
 	result, err := srv.Search.SearchInventory(params)
