edb02dee0bb592e882aa97e5a9560fb7d13edce6
Tommy Folan <tommy.folan@alaska.gov>
2025-05-16 14:39:22 -0800

    Fix mistake in query

--- assets/pg/move/validate_barcodes_to_move.sql
+++ assets/pg/move/validate_barcodes_to_move.sql
@@ -2,7 +2,7 @@ SELECT ARRAY(
     SELECT barcode
     FROM container
     WHERE barcode = ANY($1::varchar[])
-    UNION
+    UNION ALL
     SELECT barcode
     FROM inventory
     WHERE barcode = ANY($1::varchar[])
